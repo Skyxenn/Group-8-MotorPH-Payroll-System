4dd7ddbbc1bca5df394f00e5ca351bed4acc598f
gamer <gamer@Skyre>
2024-02-02 10:53:53 +0800

    first commit

---
+++ src/payroll/system/PayrollSystem.java
@@ -0,0 +1,12 @@
+package payroll.system;
+
+import java.util.Scanner;
+
+public class PayrollSystem {
+   
+    public static void main(String[] args) {
+        Scanner inp = new Scanner (System  .in);
+        
+    }
+    
+}
