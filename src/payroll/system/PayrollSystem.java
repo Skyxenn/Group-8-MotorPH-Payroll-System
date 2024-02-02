9d95919290322bf9026fc0d1873fc0ce0d4fd295
Skyxenn <158536987+Skyxenn@users.noreply.github.com>
2024-02-02 11:43:15 +0800

    Update PayrollSystem.java

--- src/payroll/system/PayrollSystem.java
+++ src/payroll/system/PayrollSystem.java
@@ -5,8 +5,41 @@ import java.util.Scanner;
 public class PayrollSystem {
    
     public static void main(String[] args) {
-        Scanner inp = new Scanner (System.in);
+        Scanner inp = new Scanner(System.in);
+        long empNum;
+        String empName;
+        double hourSalary, weeklyTime, weeklySalary, overTime;
+        double regPay, overtimePay, netPay;
+
+        System.out.printf("Enter Employee No.: ");
+        empNum = inp.nextLong();
+        System.out.printf("Enter Hourly Salary: ");
+        hourSalary = inp.nextDouble();
+        System.out.printf("Enter Weekly Time: ");
+        weeklyTime = inp.nextDouble();
         
+        if (empNum == 10001) {
+            empName = "Samuel Chua";
+        }
+        else if (empNum == 10002) {
+            empName = "John Paul";
+        }
+        else if (empNum == 10003) {
+            empName = "Timothy Magtibay";
+        }
+        else if (empNum == 10004) {
+            empName = "Jan Daniel Garcia";
+        }
+        else {
+            empName = "Unknown Employee";
+        }
+        
+        if (weeklyTime < 100) {
+            weeklySalary = weeklyTime;
+            overTime = 0;
+            regPay = hourSalary * weeklyTime;
+            overtimePay = 0;
+            netPay = regPay;
+        }
     }
-    
 }
